2518727f699115a452ed9c2c74cd7548dc58fe43
Nanwan <2485332377@qq.com>
2025-03-22 15:22:51 +0800

    The flexibility of the code is increased, and the custom service type is more convenient

--- src/main_service.cpp
+++ src/main_service.cpp
@@ -16,23 +16,7 @@ int main(int argc, char** argv) {
     ros::Duration(1).sleep();//延时启动服务通信
     multibotnet::ServiceManager service_manager;
     service_manager.init(config_file);
-    
-        // 指定远程服务名称并准备请求数据
-        std::string service_name = "/remote_set_bool";
-        std_srvs::SetBool::Request req;
-        std_srvs::SetBool::Response res;
-        req.data = true;  // 设置请求数据，例如这里设置为true
-        while (ros::ok())
-        {
-                // 调用远程服务
-            if (service_manager.callService<std_srvs::SetBool>(service_name, req, res)) {
-                ROS_INFO("OK!!!: %s", res.message.c_str());
-            } else {
-                ROS_ERROR("Fault");
-            }
-            ros::Duration(1).sleep();
-        }
-        
+
     ros::spin();
     return 0;
 }
